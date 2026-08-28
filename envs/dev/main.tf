4d1ca7f28620ae696a4d1e640719ef0019f98dbe
Bryan Pineda <bryan.pinedag@hotmail.com>
2026-08-28 00:31:33 -0600

    scaffold: workflow file

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -10,7 +10,7 @@ module "network" {
   name                = "${local.name_prefix}-vnet"
   location            = var.location
   resource_group_name = azurerm_resource_group.this.name
-  address_space = var.address_space
+  address_space       = var.address_space
   subnets             = local.subnets
   tags                = local.tags
 }
